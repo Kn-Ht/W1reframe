c6c7cbafbd64243d2e6cef9419ef5404bf649bfd
kvoid <kianheitkamp@outlook.com>
2024-01-28 22:31:38 +0100

    some scaffolding work

---
+++ src/game.rs
@@ -0,0 +1,52 @@
+use macroquad::prelude::*;
+
+#[derive(Debug, Clone, Copy)]
+pub enum GameState {
+    TitleScreen,
+    Running,
+    Paused
+}
+
+#[derive(Debug, Clone, Copy, Default)]
+pub struct GlobalState {
+    pub screen_w: f32,
+    pub screen_h: f32,
+    pub fullscreen: bool,
+}
+
+impl GlobalState {
+    fn update(&mut self) {
+        self.screen_w = screen_width();
+        self.screen_h = screen_height();
+    }
+    pub fn toggle_fullscreen(&mut self) {
+        self.fullscreen = !self.fullscreen;
+        set_fullscreen(self.fullscreen);
+    }
+}
+
+pub struct Game {
+    gamestate: GameState,
+    pub state: GlobalState
+}
+
+impl Game {
+    pub fn new() -> Self {
+        Self {
+            gamestate: GameState::TitleScreen,
+            state: GlobalState::default(),
+        }
+    }
+    /// Start the main loop by calling `callback` each frame
+    pub async fn start_loop<F: Fn(&mut Self)>(&mut self, callback: F) -> ! {
+        loop {
+            self.state.update(); // 1. Update shared state
+            callback(self);      // 2. Run user-specified code
+            next_frame().await;  // 3. Render the frame
+        }
+    }
+    /// Render the title screen and check if a button was pressed
+    pub fn render_title_screen(&mut self) {
+
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
-fn main() {
-    println!("Hello, world!");
+use macroquad::prelude::*;
+
+mod game;
+use game::Game;
+
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "W1reFrame".to_owned(),
+        window_width: 800, window_height: 600,
+        high_dpi: true,
+        fullscreen: false,
+        window_resizable: true,
+        ..Default::default()
+    }
+}
+
+
+#[macroquad::main(window_conf)]
+async fn main() {
+    let mut game = Game::new();
+
+    game.start_loop(|game| {
+        clear_background(BLACK);
+
+        egui_macroquad::ui(|egui_ctx| {
+            egui::Window::new("egui ❤ macroquad")
+                .show(egui_ctx, |ui| {
+                    ui.label(format!("FPS: {}", get_fps()));
+                    ui.label(format!("Screen size: {}x{}", game.state.screen_w, game.state.screen_h));
+                    ui.label(format!("Key W: {:?}", is_key_down(KeyCode::W)));
+                });
+        });
+
+        egui_macroquad::draw();
+    }).await;
 }
